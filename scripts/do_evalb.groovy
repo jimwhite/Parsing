bedd2fb0fdc50ecb063e249116f6ff19ad864183
Jim White <james.paul.white@gmail.com>
2013-03-13 11:23:19 -0700

    Merge evalb and nbest logp and output as tab delimited file.

--- scripts/do_evalb.groovy
+++ scripts/do_evalb.groovy
@@ -10,7 +10,7 @@ all_files = sysout_dir.listFiles().grep { it.isDirectory() }.collectMany { it.li
 
 evalb_dir = new File(tmp_dir, 'evalb')
 
-evalb(all_files, new File(evalb_dir, 'xcorpus'))
+//evalb(all_files, new File(evalb_dir, 'xcorpus'))
 
 all_files.each { File best ->
     // evalb([new File(tmp_dir, 'xcorpus/cf/cf03.mrg.best')], new File(evalb_dir, 'cf/cf03'))
@@ -22,14 +22,17 @@ all_files.each { File best ->
 
 def evalb(List<File> best_files, File baseFile)
 {
+    def nbest_trees = new File(baseFile.parentFile, baseFile.name + '.nbest')
     def reranker_output = new File(baseFile.parentFile, baseFile.name + '.best')
     def evalb_gold = new File(baseFile.parentFile, baseFile.name + '.eval')
 
     baseFile.parentFile.mkdirs()
+    nbest_trees.delete()
     reranker_output.delete()
     evalb_gold.delete()
 
     best_files.each { File best ->
+        nbest_trees << new File(best.parentFile, best.name.replaceAll(/best$/, /nbest/)).text
         reranker_output << best.text
         evalb_gold << new File(best.parentFile, best.name.replaceAll(/best$/, /eval/)).text
     }
@@ -49,4 +52,56 @@ def evalb(List<File> best_files, File baseFile)
             }
         }
     }
+
+    def logpFile = new File(baseFile.parentFile, baseFile.name + '.logp.txt')
+
+    def evalb_lines = outFile.text.readLines()
+
+    if (evalb_lines) {
+        evalb_lines.remove(0)   //   Sent.                        Matched  Bracket   Cross        Correct Tag
+        evalb_lines.remove(0)   // ID  Len.  Stat. Recal  Prec.  Bracket gold test Bracket Words  Tags Accracy
+        def header_separator = evalb_lines.remove(0)
+        if (!header_separator.startsWith('===')) {
+            println "Expected ${outFile.path} to be separator but got this instead:"
+            println header_separator
+            println()
+        }
+
+        logpFile.withPrintWriter { printer ->
+            def parse_lines = nbest_trees.readLines() // .collect { it.trim() }.grep { it }
+
+            while (parse_lines) {
+                def h = parse_lines.remove(0)
+
+                if (h) {
+                    def (_, parse_count, sentence_id0) = (h =~ /(\d+)[^.]+\.(.+)/)[0]
+                    parse_count = parse_count as Integer
+
+                    def first_p = 0
+
+                    parse_count.times { i ->
+                        def p = parse_lines.remove(0) as Double
+                        def x = parse_lines.remove(0)
+
+                        if (!i) first_p = p
+                    }
+
+                    parse_lines.remove(0)
+
+                    def evalb_line = evalb_lines.remove(0)
+                    def eval_matcher = evalb_line =~ /\s*(\d+)\s+(\d+)\s+(\d+)\s+([.\d]+)\s+([.\d]+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+([.\d]+)/
+                    def eval_match = eval_matcher[0]
+                    def (__, sentence_id1, sent_len, status, recall, precision, matched_bracket, brackets_gold, brackets_test, cross_bracket, word_count, correct_tags, tag_accuracy) = eval_match
+
+                    sentence_id0 = sentence_id0 as Integer
+                    sentence_id1 = sentence_id1 as Integer
+                    if (((status as Integer) == 0) && (sentence_id0 == sentence_id1 - 1)) {
+                        printer.println "${eval_match.join('\t')}\t$first_p"
+//                    printer.println "$sentence_id1\t$sent_len\t${sentence_id1-sentence_id0}\t$recall\t$precision\t$first_p"
+                    }
+                }
+            }
+        }
+    }
+
 }
