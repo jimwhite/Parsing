59be5a7fc063a40d52a440d9be14500a60b8fcbb
Jim White <james.paul.white@gmail.com>
2013-03-17 03:33:25 -0700

    Tidy up gondor experiments script a little.

--- scripts/experiments.groovy
+++ scripts/experiments.groovy
@@ -4,6 +4,11 @@
 // Environmental Dependencies
 /////////////////////////////
 
+// If there are environment variables you want to copy from the current process, use clone_environment:
+// gondor.clone_environment('PATH', 'ANT_HOME', 'JAVA_HOME')
+// If you want to copy *all* of the the current environment variables, omit the variable names (not recommended):
+// gondor.clone_environment()
+
 gondor.environment = [PATH:"/usr/kerberos/bin:/usr/local/bin:/bin:/usr/bin:/opt/git/bin:/opt/scripts:/condor/bin"
         , LC_COLLATE:'C'  // first-stage/PARSE/parseIt needs this to deal with punctuation correctly!
                             // Not really.  Need to use -K (pretokenized) flag.
@@ -30,12 +35,14 @@ convert_ptb = gondor.condor_command(new File(bllip_dir, 'second-stage/programs/p
 PARSER_MODEL=new File(bllip_dir, 'first-stage/DATA/EN/')
 MODELDIR=new File(bllip_dir, 'second-stage/models/ec50spnonfinal')
 ESTIMATORNICKNAME='cvlm-l1c10P1'
+RERANKER_WEIGHTS = new File(MODELDIR, ESTIMATORNICKNAME + '-weights.gz')
+RERANKER_FEATURES = new File(MODELDIR, 'features.gz')
 
 // first-stage/PARSE/parseIt -l399 -N50 first-stage/DATA/EN/ $*
 parse_nbest = gondor.condor_command(new File(bllip_dir, 'first-stage/PARSE/parseIt'), ['-K.flag', '-l400.flag', '-N50.flag', 'model.in', 'input.in'])
 
 // second-stage/programs/features/best-parses" -l "$MODELDIR/features.gz" "$MODELDIR/$ESTIMATORNICKNAME-weights.gz"
-parse_rerank = gondor.condor_command(new File(bllip_dir, 'second-stage/programs/features/best-parses'), ['-l.flag', 'features.in', 'weights.in', 'infile.in'])
+rerank_parses = gondor.condor_command(new File(bllip_dir, 'second-stage/programs/features/best-parses'), ['-l.flag', 'features.in', 'weights.in', 'infile.in'])
 
 //////////////////////
 // Job DAG Definitions
@@ -44,17 +51,20 @@ parse_rerank = gondor.condor_command(new File(bllip_dir, 'second-stage/programs/
 println test_original_ptb_files
 
 test_original_ptb_files.each { File original_ptb ->
-    def charniak_input = new File(tmp_dir, original_ptb.path + ".sent")
-    def evalb_gold = new File(tmp_dir, original_ptb.path + ".eval")
-    def nbest_output = new File(tmp_dir, original_ptb.path + '.nbest')
-    def reranker_output = new File(tmp_dir, original_ptb.path + '.best')
+    def work_dir = new File(tmp_dir, original_ptb.parentFile.path)
+    work_dir.mkdirs()
 
-    charniak_input.parentFile.mkdirs()
-    evalb_gold.parentFile.mkdirs()
+    def charniak_input = new File(work_dir, original_ptb.name + ".sent")
+    def evalb_gold = new File(work_dir, original_ptb.name + ".eval")
+    def nbest_output = new File(work_dir, original_ptb.name + '.nbest')
+    def reranker_output = new File(work_dir, original_ptb.name + '.best')
 
     if (!charniak_input.exists()) convert_ptb(mode:'-c', from:original_ptb, outfile:charniak_input)
     if (!evalb_gold.exists()) convert_ptb(mode:'-e', from:original_ptb, outfile:evalb_gold)
 
+    // parse_nbest(model:PARSER_MODEL, input:charniak_input) >> tee(nbest_output) >> rerank_parses(features: RERANKER_FEATURES, weights: RERANKER_WEIGHTS, outfile:reranker_output)
+    // charniak_input >> parse_nbest(model:PARSER_MODEL) >> nbest_output >> rerank_parses(features: RERANKER_FEATURES, weights: RERANKER_WEIGHTS) >> reranker_output
+
     parse_nbest(model:PARSER_MODEL, input:charniak_input, outfile:nbest_output)
-    parse_rerank(features:new File(MODELDIR, 'features.gz'), weights:new File(MODELDIR, ESTIMATORNICKNAME+'-weights.gz'), infile:nbest_output, outfile:reranker_output)
+    rerank_parses(features: RERANKER_FEATURES, weights: RERANKER_WEIGHTS, infile:nbest_output, outfile:reranker_output)
 }
