4d25278974e5e0d5ae79d5838f6938b56a24b2f2
Jim White <james.paul.white@gmail.com>
2013-03-18 04:43:34 -0700

    Just strip S1 to be like PTB .mrg files.

--- scripts/prep_self_training.groovy
+++ scripts/prep_self_training.groovy
@@ -1,34 +1,20 @@
 #!/usr/bin/env groovy
 
 keepers = args[0] as Integer
-skippers = args[1] as Integer
+period = args[1] as Integer
 
-bllip_dir = new File('bllip-parser')
+def best = new File(args[2])
 
-tmp_dir = new File('tmp')
+int i = keepers
+int j = period
 
-sysout_dir = new File(tmp_dir, 'xcorpus')
+best.eachLine { line ->
+    if (i-- > 0) {
+        println(line.replaceFirst(/^\(S1/, "("))
+    }
 
-all_files = sysout_dir.listFiles().grep { it.isDirectory() }.collectMany { it.listFiles().grep { it.name.endsWith ".best" } }
-
-training_data_dir = new File('xtrain')
-training_data_dir.delete()
-training_data_dir.mkdirs()
-
-all_files.each { File best ->
-    def training_file = new File(training_data_dir, (best.name - ~/\.best$/))
-    training_file.withPrintWriter { printer ->
-        int i = keepers
-        int j = skippers
-        best.eachLine { line ->
-            if (--i >= 0) {
-                printer.println (line.replaceFirst(/^\(S1/, "("))
-            } else {
-                if (--j < 1) {
-                    i = keepers
-                    j = skippers
-                }
-            }
-        }
+    if (--j < 1) {
+        i = keepers
+        j = period
     }
 }
