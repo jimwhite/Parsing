9d544032f90f73bc18b41ac9942d26c91e3b1bfc
Jim White <james.paul.white@gmail.com>
2013-03-18 10:51:18 -0700

    Full-fledged self-train preparer - uniform only.

--- scripts/prep_self_training.groovy
+++ scripts/prep_self_training.groovy
@@ -1,20 +1,35 @@
 #!/usr/bin/env groovy
 
-keepers = args[0] as Integer
-period = args[1] as Integer
+parsed_source = args[0]
+measure = args[1]
+endess = args[2]
+percentage = args[3] as Integer
+file_base = args[4]
 
-def best = new File(args[2])
+assert measure=='uni'
+assert (percentage % 10 == 0)
+
+keepers = percentage / 10
+period = 10
+
+def xtrain_dir = new File("bllip-parser/xtrain")
+xtrain_dir.mkdirs()
+
+def best_file = new File(parsed_source, "tmp/parsed/${file_base}.best")
+def xtrain_mrg_file = new File(xtrain_dir, file_base + ".mrg")
 
 int i = keepers
 int j = period
 
-best.eachLine { line ->
-    if (i-- > 0) {
-        println(line.replaceFirst(/^\(S1/, "("))
-    }
+xtrain_mrg_file.withPrintWriter { printer ->
+    best_file.eachLine { line ->
+        if (i-- > 0) {
+            printer.println(line.replaceFirst(/^\(S1/, "("))
+        }
 
-    if (--j < 1) {
-        i = keepers
-        j = period
+        if (--j < 1) {
+            i = keepers
+            j = period
+        }
     }
 }
