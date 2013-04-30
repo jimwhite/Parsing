4383441fd18d59fe9d679bcdf34a659b917a32bd
Jim White <james.paul.white@gmail.com>
2013-04-30 04:56:29 -0700

    Select a random parse for each sentence from the ensemble and use it to eval them all.

--- scripts/ensemble_sepa.groovy
+++ scripts/ensemble_sepa.groovy
@@ -5,7 +5,7 @@ workspace_dir = new File('/home2/jimwhite/workspace/parsers')
 mrg_file_name = 'brown-train.mrg'
 
 // Each parser has it's own binary, but we'll use the one in base for them all.
-bllip_dir = new File(workspace_dir, 'base/bllip-parser')
+bllip_parser_dir = new File(workspace_dir, 'base/bllip-parser')
 
 ensemble_dir = new File(workspace_dir, 'ensemble')
 
@@ -35,3 +35,47 @@ selected_best_file.withPrintWriter { printer ->
 
 best_parse_readers.each { it.close() }
 
+selected_evalb_gold_file = new File(ensemble_dir, mrg_file_name + '.1ofE.eval')
+
+prepare_ptb('-e', selected_best_file, selected_evalb_gold_file)
+
+parser_dir_list.each { File parser_dir ->
+    def sysout_dir = new File(parser_dir, 'tmp/parsed')
+    evalb(selected_best_file, new File(sysout_dir, mrg_file_name + '.best'), new File(sysout_dir, mrg_file_name + '.evalb.txt'))
+}
+
+def prepare_ptb(String switches, File src_file, File dst_file)
+{
+    def command = ['second-stage/programs/prepare-data/ptb', switches, src_file]
+
+    println command
+
+    dst_file.withOutputStream { stdout ->
+        def stderr = new ByteArrayOutputStream()
+        def proc = command.execute(null, bllip_parser_dir)
+        proc.waitForProcessOutput(stdout, stderr)
+        if (stderr.size()) System.err.println(stderr.toString())
+        if (proc.exitValue()) {
+            System.err.println "exitValue: ${proc.exitValue()}"
+        }
+    }
+}
+
+def evalb(File evalb_gold, File best_file, File outFile)
+{
+    def errFile = new File(outFile.parentFile, (outFile.name - ~/\.txt$/)+ '.err')
+
+    def command = ['evalb/evalb', '-p', 'evalb/new.prm', evalb_gold, best_file]
+
+    println command
+
+    outFile.withOutputStream { stdout ->
+        errFile.withOutputStream { stderr ->
+            def proc = command.execute(null, bllip_parser_dir)
+            proc.waitForProcessOutput(stdout, stderr)
+            if (proc.exitValue()) {
+                println "exitValue: ${proc.exitValue()}"
+            }
+        }
+    }
+}
