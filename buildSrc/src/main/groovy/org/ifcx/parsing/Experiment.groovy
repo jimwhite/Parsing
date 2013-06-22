d2891848b8598cdc1824690f391b2d53ccb9dfa4
Jim White <james.paul.white@gmail.com>
2013-06-22 13:34:59 -0700

    Can't use Gradle's Copy task because it won't preserve timestamps (which we need to avoid remaking executables).

--- buildSrc/src/main/groovy/org/ifcx/parsing/Experiment.groovy
+++ buildSrc/src/main/groovy/org/ifcx/parsing/Experiment.groovy
@@ -4,11 +4,13 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.Exec
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.TaskExecutionException
 
 class Experiment extends DefaultTask {
     @InputDirectory
@@ -23,18 +25,25 @@ class Experiment extends DefaultTask {
     @TaskAction
     void create()
     {
-        experiment_dir.mkdirs()
-
         def parser_dir = new File(experiment_dir, base_parser_dir.name)
 
-        def copy_base_parser = project.task(name + '-copy-base-parser', type:Copy) {
-            from(base_parser_dir) {
-                exclude 'xtrain/*'
-                exclude 'xtune/*'
-            }
-            destinationDir parser_dir
+        if (!parser_dir.exists()) {
+            if (!parser_dir.mkdirs()) throw new IOException("Failed to create experiment/parser dir $parser_dir")
         }
-        copy_base_parser.execute()
+
+        // Can't use Gradle's copy task because it won't preserve timestamps.
+//        def copy_base_parser = project.task(name + '-copy-base-parser', type:Copy) {
+//            from(base_parser_dir) {
+//                exclude 'xtrain/*'
+//                exclude 'xtune/*'
+//            }
+//            destinationDir parser_dir
+//        }
+
+        ["first-stage", "second-stage", "evalb", "Makefile", "train_all.sh", "train_all.condor"].each {
+            def result = copy_files(new File(base_parser_dir, it), new File(parser_dir, it))
+            if (result) throw new IOException("Failed to copy files form $base_parser_dir to $parser_dir")
+        }
 
         def corpus_project = project.project(corpus_name)
 
@@ -48,5 +57,16 @@ class Experiment extends DefaultTask {
             copy_data.execute()
         }
     }
+
+    static def copy_files(File src_dir, File dst_dir)
+    {
+        String cmd = "cp -RLp $src_dir $dst_dir"
+        def proc = cmd.execute()
+        if (proc.waitFor()) {
+            print "Error: '$cmd' = ${proc.exitValue()}"
+        }
+        proc.exitValue()
+    }
+
 }
 
