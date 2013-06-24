29b40c63a1a531e4b53390d851c828a69eccf571
Jim White <james.paul.white@gmail.com>
2013-06-23 18:00:28 -0700

    New and improved Ant-based SetUpParser (was SetupExperiment).

--- buildSrc/src/main/groovy/org/ifcx/parsing/Experiment.groovy
+++ buildSrc/src/main/groovy/org/ifcx/parsing/Experiment.groovy
@@ -1,72 +1,28 @@
 package org.ifcx.parsing
 
-import org.gradle.api.DefaultTask
-import org.gradle.api.Project
 import org.gradle.api.Task
-import org.gradle.api.tasks.Copy
-import org.gradle.api.tasks.Exec
-import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.InputDirectory
-import org.gradle.api.tasks.InputFiles
-import org.gradle.api.tasks.OutputDirectory
-import org.gradle.api.tasks.TaskAction
-import org.gradle.api.tasks.TaskExecutionException
 
-class Experiment extends DefaultTask {
-    @InputDirectory
-    File base_parser_dir
+class Experiment
+{
+    String name
 
-    @Input
-    String corpus_name
-
-    @OutputDirectory
     File experiment_dir
 
-    @TaskAction
-    void create()
-    {
-        def parser_dir = new File(experiment_dir, base_parser_dir.name)
+    def corpus
 
-        if (!parser_dir.exists()) {
-            if (!parser_dir.mkdirs()) throw new IOException("Failed to create experiment/parser dir $parser_dir")
-        }
+    def parser
 
-        // Can't use Gradle's copy task because it won't preserve timestamps.
-//        def copy_base_parser = project.task(name + '-copy-base-parser', type:Copy) {
-//            from(base_parser_dir) {
-//                exclude 'xtrain/*'
-//                exclude 'xtune/*'
-//            }
-//            destinationDir parser_dir
-//        }
+    def parameters
 
-        ["first-stage", "second-stage", "evalb", "Makefile", "train_all.sh", "train_all.condor"].each {
-            def result = copy_files(new File(base_parser_dir, it), new File(parser_dir, it))
-            if (result) throw new IOException("Failed to copy files form $base_parser_dir to $parser_dir")
-        }
+    File parser_dir
 
-        def corpus_project = project.project(corpus_name)
+    File model_dir
 
-        ['train', 'tune', 'test'].each { split_name ->
-            def mrg_source_files = corpus_project.tasks[split_name + '_MRG'].outputs.files
-            def dest_dir = new File(parser_dir, 'x' + split_name)
-            def copy_data = project.task("$name-copy-$split_name", type:Copy) {
-                from mrg_source_files
-                destinationDir dest_dir
-            }
-            copy_data.execute()
-        }
-    }
+    Task setup_task
 
-    static def copy_files(File src_dir, File dst_dir)
-    {
-        String cmd = "cp -RLp $src_dir $dst_dir"
-        def proc = cmd.execute()
-        if (proc.waitFor()) {
-            print "Error: '$cmd' = ${proc.exitValue()}"
-        }
-        proc.exitValue()
-    }
+    Task train_task
+
+    Task test_task
+
 
 }
-
---
+++ buildSrc/src/main/groovy/org/ifcx/parsing/SetUpParser.groovy
@@ -0,0 +1,126 @@
+package org.ifcx.parsing
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.TaskAction
+
+class SetUpParser extends DefaultTask {
+    @InputDirectory
+    File base_parser_dir
+
+    @Input
+    String corpus_name
+
+    @OutputDirectory
+    File parser_dir
+
+    def train_mrg
+
+    def tune_mrg
+
+    @TaskAction
+    void setup()
+    {
+        def tmpDir = new File(parser_dir, 'tmp')
+
+        ant.sequential {
+            mkdir(dir:parser_dir)
+
+            mkdir(dir:tmpDir)
+
+            exec(executable:'cp') {
+                arg(line:'-RLp')
+                ['first-stage', 'second-stage', 'evalb', 'Makefile', 'train_all.sh', 'train_all.condor'].each {
+                    arg(file:new File(base_parser_dir, it))
+                }
+                arg(file:parser_dir)
+            }
+        }
+
+        def corpus_tasks = project.project(corpus_name).tasks
+
+//        [[corpus_tasks.'train_MRG', 'train'], [corpus_tasks.'tune_MRG', 'tune'], [corpus_tasks.'test_MRG', 'test']].each { corpus_task, split_name ->
+
+        ['train', 'tune', 'test'].each { split_name ->
+            def corpus_task = corpus_tasks[split_name + '_MRG']
+            def mrg_source_files = corpus_task.outputs.files
+
+            ant.sequential {
+                copy(todir:new File(parser_dir, split_name), preservelastmodified:true) {
+                    mrg_source_files.each { f ->
+                        filelist(dir:f.parentFile) { file(name:f.name) }
+                    }
+                }
+
+                concat(destfile:new File(tmpDir, split_name + '-all.mrg'), eol:'unix') {
+                    mrg_source_files.each { f ->
+                        filelist(dir:f.parentFile) { file(name:f.name) }
+                    }
+                }
+            }
+
+            // Too cool for school one-liner:
+            // new File(tmpDir, split_name + '-all-files.txt').write(mrg_source_files*.path.join('\n'))
+
+            // The sensible expanded version...
+            new File(tmpDir, split_name + '-all-files.txt').withPrintWriter { listing ->
+                mrg_source_files.each { listing.println it.path }
+            }
+        }
+
+//            copy(todir:parser_dir, preservelastmodified:true) {
+//                fileset(dir:base_parser_dir) {
+//                    include(name:'first-stage/**')
+//                    include(name:'second-stage/**')
+//                    include(name:'evalb/**')
+//                    include(name:'Makefile')
+//                    include(name:'train_all.sh')
+//                    include(name:'train_all.condor')
+//                }
+//            }
+
+//        if (!parser_dir.exists()) {
+//            if (!parser_dir.mkdirs()) throw new IOException("Failed to create experiment/parser dir $parser_dir")
+//        }
+//
+        // Can't use Gradle's copy task because it won't preserve timestamps.
+//        def copy_base_parser = project.task(name + '-copy-base-parser', type:Copy) {
+//            from(base_parser_dir) {
+//                exclude 'xtrain/*'
+//                exclude 'xtune/*'
+//            }
+//            destinationDir parser_dir
+//        }
+
+//        ["first-stage", "second-stage", "evalb", "Makefile", "train_all.sh", "train_all.condor"].each {
+//            def exitCode = copy_files(new File(base_parser_dir, it), new File(parser_dir, it))
+//            if (exitCode) throw new IOException("Failed to copy files form $base_parser_dir to $parser_dir (exitCode = $exitCode)")
+//        }
+
+//        def corpus_project = project.project(corpus_name)
+//
+//        ['train', 'tune', 'test'].each { split_name ->
+//            def mrg_source_files = corpus_project.tasks[split_name + '_MRG'].outputs.files
+//            def dest_dir = new File(parser_dir, 'x' + split_name)
+//            def copy_data = project.task("$name-copy-$split_name", type:Copy) {
+//                from mrg_source_files
+//                destinationDir dest_dir
+//            }
+//            copy_data.execute()
+//        }
+    }
+
+//    static def copy_files(File src_dir, File dst_dir)
+//    {
+//        String cmd = "cp -RLp $src_dir $dst_dir"
+//        def proc = cmd.execute()
+//        if (proc.waitFor()) {
+//            print "Error: '$cmd' = ${proc.exitValue()}"
+//        }
+//        proc.exitValue()
+//    }
+
+}
+
