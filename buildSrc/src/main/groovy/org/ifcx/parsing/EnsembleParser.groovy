c23f03a3f5f78062121a66574a3e039d51355e2c
Jim White <james.paul.white@gmail.com>
2013-06-26 17:36:04 -0700

    Ensemble set up task outputs trees one per line.

--- buildSrc/src/main/groovy/org/ifcx/parsing/EnsembleParser.groovy
+++ buildSrc/src/main/groovy/org/ifcx/parsing/EnsembleParser.groovy
@@ -40,22 +40,84 @@ class EnsembleParser
                 inputs.files.add(all_mrg_files)
 
                 def combined_mrg_file = new File(tmpDir, 'train-tune-all.mrg')
+                def one_per_line_mrg_file = new File(tmpDir, 'train-tune-all-one-per-line.mrg')
 
                 outputs.files(combined_mrg_file)
+                outputs.files(one_per_line_mrg_file)
 
                 doLast {
-//                    ant.concat(destfile: combined_mrg_file, eol:'unix') {
-//                        all_mrg_files.each { f ->
-//                            filelist(dir:f.parentFile) { file(name:f.name) }
-//                        }
-//                    }
                     ant.concat(destfile: combined_mrg_file, eol:'unix') {
                         // Would like to say this something like this way:
-                        // filelist(files:all_mrg_files.files)
+                        // resources(all_mrg_files.files)
                         all_mrg_files.addToAntBuilder(ant, 'resources')
+                    }
+
+                    // Flatten the parse trees (s-expressions) so that they are one per line.
+                    one_per_line_mrg_file.withPrintWriter { mrg_writer ->
+                    combined_mrg_file.withReader { mrg_reader ->
+                        def sexp
+                        while (sexp = read_one_sexp(mrg_reader)) {
+                            mrg_writer.println sexp_to_string(sexp)
+                        }
+                    }
                     }
                 }
             }
         }
+    }
+
+    static def read_one_sexp(Reader reader)
+    {
+        final tokenDelimiters = "()\t\r\n "
+
+        List stack = []
+        List sexps = []
+
+        def cint = reader.read()
+
+        loop:
+        while (cint >= 0) {
+            Character c = cint as Character
+            switch (c) {
+                case ')' :
+                    // End of sexp with without beginning.
+                    // Print a warning?
+                    if (stack.size() < 1) break loop
+
+                    sexps = stack.pop() << sexps
+
+                    // We read only one complete sexp.
+                    if (stack.size() < 1) break loop
+
+                    cint = reader.read()
+                    break
+
+                case '(':
+                    stack.push(sexps)
+                    sexps = []
+                    cint = reader.read()
+                    break
+
+                default:
+                    if (c.isWhitespace() || c == 0 || c == 26 /* ASCII EOF */ ) {
+                        cint = reader.read()
+                    } else {
+                        def token = new StringBuilder()
+                        token.append(c)
+                        while ((cint = reader.read()) >= 0) {
+                            if (tokenDelimiters.indexOf(cint) >= 0) break
+                            token.append(cint as Character)
+                        }
+                        sexps << token.toString()
+                    }
+            }
+        }
+
+        return sexps ? sexps[0] : null
+    }
+
+    static def sexp_to_string(sexp)
+    {
+        (sexp instanceof List) ? "(${sexp.collect { sexp_to_string(it) }.join(' ')})" : sexp.toString()
     }
 }
