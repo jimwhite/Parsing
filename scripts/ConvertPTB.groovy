7d50ab35b70a0b7f2d76ce8f533505e4c536c53d
Jim White <james.paul.white@gmail.com>
2014-07-17 00:28:13 -0700

    PTB converter command.

---
+++ scripts/ConvertPTB.groovy
@@ -0,0 +1,37 @@
+import com.beust.jcommander.Parameter
+import groovy.transform.Field
+import org.ifcx.gondor.Command
+import org.ifcx.gondor.WorkflowScript
+import org.ifcx.gondor.api.InputDirectory
+import org.ifcx.gondor.api.InputFile
+import org.ifcx.gondor.api.OutputFile
+
+@groovy.transform.BaseScript WorkflowScript thisScript
+
+@Parameter(names = '--format', description = 'Output format: gold (-e) or sentence (-c).')
+@Field String mode
+
+@Parameter(names = '--input', description = 'PTB input file.')
+@InputFile @Field File input
+
+@Parameter(names = '--output', description = 'Converted output file.')
+@OutputFile @Field File output
+
+def bllip_parser_dir = new File('../bllip-parser')
+def ptb_executable = new File(bllip_parser_dir, 'second-stage/programs/prepare-data/ptb')
+
+def ptb_convert = command(path:ptb_executable.path) {
+    // Flag that is set for pre-tokenized input.
+    arg 'mode', Command.REQUIRED, {
+        switch (it) {
+            case 'tree' : '-e'; break
+            case 'text' : '-c'; break
+            default : throw new IllegalArgumentException("mode arg to PTB converter must be 'tree' or 'text' but got $it");
+        }
+    }
+
+    // PTB input file
+    infile 'input'
+}
+
+ptb_convert(mode:mode, input:input) >> output
